7aa11a5cdfde4135751bb1928c6d51fd67c806a6
Fernando Gadea Martínez <cursofgm@gmail.com>
2017-03-03 12:35:45 +0100

    Actualización de ubicación

--- js/control.js
+++ js/control.js
@@ -1,17 +1,18 @@
-function ElementoId (id) {
-	return document.getElementById(id);
+function ElementoId(id) {
+    return document.getElementById(id);
 };
 
-function ComprobarLocalStorage () {
-	if (typeof(Storage) !== "undefined") {
-    // Local Storage correcto.
-} 
-else {
-    // Lo sentimos, no soporta Local storage...
-}
+function ComprobarLocalStorage() {
+    if (typeof(Storage) !== "undefined") {
+        // Local Storage correcto.
+    } else {
+        // Lo sentimos, no soporta Local storage...
+    }
 }
 
-function Guardar(){
-	localStorage.setItem("nombre", ElementoId("nombre").value);
-	
+ComprobarLocalStorage();
+
+function Guardar() {
+    localStorage.setItem("nombre", ElementoId("nombre").value);
+
 }
