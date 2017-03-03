f88aece6d0004610f31f9aa08e3e5c218f97e5b8
Fernando Gadea Martínez <cursofgm@gmail.com>
2017-03-03 14:16:43 +0100

    Incluida toma de datos.

--- js/control.js
+++ js/control.js
@@ -14,5 +14,12 @@ ComprobarLocalStorage();
 
 function Guardar() {
     localStorage.setItem("nombre", ElementoId("nombre").value);
+    localStorage.setItem("apellido1", ElementoId("apellido1").value);
+    localStorage.setItem("apellido2", ElementoId("apellido2").value);
+    localStorage.setItem("dir", ElementoId("dir").value);
+    localStorage.setItem("pob", ElementoId("pob").value);
+    localStorage.setItem("cp", ElementoId("cp").value);
+    localStorage.setItem("mail", ElementoId("mail").value);
+
 
 }
